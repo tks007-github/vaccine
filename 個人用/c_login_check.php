8ac16e2cb15624763a348659a848e780fd5769dc
tks007-github <takeshi.n.2020@gmail.com>
2021-06-24 14:59:18 +0900

    update

--- 個人用/c_login_check.php
+++ 個人用/c_login_check.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>c_login_check</title>
+    <title>ワクチン予約</title>
 </head>
 <body>
 <?php
@@ -39,7 +39,7 @@ $rec=$stmt->fetch(PDO::FETCH_ASSOC);
 
 if($rec==false)	# データベースからの問合せ結果がない場合
 {
-	header('Location:c_ng.html');
+	header('Location:c_login_ng.html');
 }
 else	# データベースからの問合せがあった場合
 {

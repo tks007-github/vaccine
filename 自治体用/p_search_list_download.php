3cae5dbacd75a7f912b6cf6cbc3b2697e601a532
tks007-github <takeshi.n.2020@gmail.com>
2021-06-30 11:26:26 +0900

    update

--- 自治体用/p_search_list_download.php
+++ 自治体用/p_search_list_download.php
@@ -1,14 +1,14 @@
 <?php
-session_start();                        # p_login_check.phpで作成したセッションを再開
-session_regenerate_id(true);            # 既存のセッションIDを新しく置き換える
-if (isset($_SESSION['login']) == false)      # セッション変数loginに値が格納されていない場合
+session_start();                        // p_login_check.phpで作成したセッションを再開
+session_regenerate_id(true);            // 既存のセッションIDを新しく置き換える
+if (isset($_SESSION['login']) == false)      // セッション変数loginに値が格納されていない場合
 {
       print 'ログインされていません。<br>';
       print '<a href="p_login.html">ログイン画面へ</a>';
       exit();
 }
 
-# p_search_list.phpから渡された値を$_POSTで受け取る
+// p_search_list.phpから渡された値を$_POSTで受け取る
 $csv = $_POST['csv'];
 $file = fopen('./' . date('YmdHis') . 'list.csv', 'w');
 $csv = mb_convert_encoding($csv, 'SJIS', 'UTF-8');
@@ -73,7 +73,7 @@ fclose($file);
                   <div class="navbar-brand">
                         <?php
                         if (isset($_SESSION['login']) == true) {
-                              print $_SESSION['pre_name'];      # セッション変数pre_nameを表示
+                              print $_SESSION['pre_name'];      // セッション変数pre_nameを表示
                               print 'でログイン中';
                         }
                         ?>

a6f31c47dc76e2906fd661111024b29cf26d8c29
tks007-github <takeshi.n.2020@gmail.com>
2021-07-01 16:05:00 +0900

    update

--- 接種会場用/s_change_done.php
+++ 接種会場用/s_change_done.php
@@ -1,7 +1,7 @@
 <?php
-session_start();                        # p_login_check.phpで作成したセッションを再開
-session_regenerate_id(true);            # 既存のセッションIDを新しく置き換える
-if (isset($_SESSION['login']) == false)      # セッション変数loginに値が格納されていない場合
+session_start();                        // s_login_check.phpで作成したセッションを再開
+session_regenerate_id(true);            // 既存のセッションIDを新しく置き換える
+if (isset($_SESSION['login']) == false)      // セッション変数loginに値が格納されていない場合
 {
     print 'ログインされていません。<br>';
     print '<a href="s_login.html">ログイン画面へ</a>';
@@ -66,7 +66,7 @@ if (isset($_SESSION['login']) == false)      # セッション変数loginに値�
             <div class="navbar-brand">
                 <?php
                 if (isset($_SESSION['login']) == true) {
-                    print $_SESSION['site_name'];      # セッション変数pre_nameを表示
+                    print $_SESSION['site_name'];      // セッション変数pre_nameを表示
                     print 'でログイン中';
                 }
 
@@ -74,7 +74,8 @@ if (isset($_SESSION['login']) == false)      # セッション変数loginに値�
                 try {
                     $vac_sta_code = $_POST['vac_sta_code'];
                     $my_num = $_POST['my_num'];
-                    //データベースに接続
+                    
+                    // データベースに接続
                     $dsn = 'mysql:dbname=Vaccine_Reservation;host=localhost;charset=utf8';
                     $user = 'root';
                     $password = 'root';
@@ -82,24 +83,24 @@ if (isset($_SESSION['login']) == false)      # セッション変数loginに値�
                     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                     $sql = '
-        UPDATE Reservation
-        SET vac_sta_code = ?
-        WHERE my_num = ?
-        ';
+                            UPDATE Reservation
+                            SET vac_sta_code = ?
+                            WHERE my_num = ?
+                            ';
 
                     $stmt = $dbh->prepare($sql);
                     $data[] = $vac_sta_code;
                     $data[] = $my_num;
-                    //SQL実行
+                    
+                    // SQL実行
                     $stmt->execute($data);
 
-                    //データを取得（PDO::FETCH_ASSOCで連想配列を返す）
-                    //$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-                    //var_dump($rec);
-                    //データベース接続を切断
                     $dbh = null;
-                } catch (Exception $e) {
-                    var_dump($e);
+
+                } 
+                
+                // エラー処理
+                catch (Exception $e) {
                     print 'ただいま障害により大変ご迷惑をお掛けしております。';
                     exit();
                 }

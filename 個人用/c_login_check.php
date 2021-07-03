a9a61b24b5fb9e94bcf78a10ecf2c61d7f32f46d
tks007-github <takeshi.n.2020@gmail.com>
2021-07-03 13:16:54 +0900

    update

--- 個人用/c_login_check.php
+++ 個人用/c_login_check.php
@@ -39,9 +39,15 @@ try
 	$data[] = $birth;
 	$stmt -> execute($data);
 
+	$sql2 = 'SELECT my_num FROM Reservation WHERE my_num=?';
+	$stmt2 = $dbh -> prepare($sql2);
+	$data2[] = $my_num;
+	$stmt2 -> execute($data2);
+
 	// Vaccine_Reservationデータベースから切断する
 	$dbh=null;
 	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+	$rec2=$stmt2->fetch(PDO::FETCH_ASSOC);
 
 	if($rec==false)		// データベースからの問合せ結果がない場合
 	{
@@ -49,10 +55,15 @@ try
 	}
 	else				// データベースからの問合せがあった場合
 	{
-		$_SESSION['my_num']=$my_num;
-		$_SESSION['birth']=$birth;
-		header('Location:c_add.php');
-		exit();
+		if($rec2==false) {
+			$_SESSION['my_num']=$my_num;
+			$_SESSION['birth']=$birth;
+			header('Location:c_add.php');
+			exit();
+		}
+		else {
+			header('Location:c_login_ng2.html');
+		}
 	}
 
 	}
